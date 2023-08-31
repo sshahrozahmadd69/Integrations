7ef9378bed9c305f9de9def8a1aa033c473ecb6e
Shahroz Ahmad <sshahroz.ahmadd@gmail.com>
2023-08-31 14:28:55 +0500

    formatDescription

--- src/Controllers/trello/bugTrello.js
+++ src/Controllers/trello/bugTrello.js
@@ -4,13 +4,22 @@ const axios = require("axios");
 exports.sendBugTrello = async (content) => {
     const { apiKey, apiToken, boardId, listId, name, desc} = JSON.parse(content);
     console.log(apiKey, apiToken, boardId, listId, name, desc);
-    
+     
+    const formattedDescription = desc.map(item => {
+      return Object.entries(item)
+        .map(([key, value]) => `${key}:  ${value}`)
+          .join('\n');
+        });
+        // Combine the formatted description strings into a single string
+        const formattedDescriptionString = formattedDescription.join('\n\n');
+        console.log(formattedDescriptionString);
+
     try {
       const response = await axios.post(
         `https://api.trello.com/1/cards?key=${apiKey}&token=${apiToken}`,
         {
           name: name,
-          desc: JSON.stringify(desc),
+          desc: formattedDescriptionString,
           idList: listId,
           idBoard: boardId
         }
